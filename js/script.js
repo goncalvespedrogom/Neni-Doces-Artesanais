7a3659db2bda9b69914b52b49a219adeca262e80
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2024-10-22 12:05:06 -0300

    Add menu mobile + ajustes responsivo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 $(document).ready(function() {
-    $('#mobile-btn').on('click', function() {
-        $('#mobile-menu').toggleClass('active')
-        $('#mobile-btn').find('i').toggleClass('fa-x')
+    ScrollReveal().reveal('#cta', {
+        origin: 'left', 
+        duration: 2000,
+        distance: '20%',
     });
+
+    ScrollReveal().reveal('.dish', {
+        origin: 'left', 
+        duration: 2000,
+        distance: '20%',
+    });
+
+    ScrollReveal().reveal('#testimonial-chef', {
+        origin: 'left', 
+        duration: 1000,
+        distance: '20%',
+    });
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right', 
+        duration: 1000,
+        distance: '20%',
+    });
+});
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+});
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
 });
